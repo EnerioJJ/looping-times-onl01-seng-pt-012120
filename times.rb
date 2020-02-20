4cbd529d211fd144dd731e58b604c0e43702e0e1
Jay W <januarydwilson@gmail.com>
2020-02-20 20:52:03 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,13 +1,7 @@
 def using_times
-	motorcycles = 200
-	
 	3.times do
-	  puts "Repairing 25 bikes today."
-	  
-	  motorcycles -= 25
+	  puts "Wingardium Leviosa"
 	end
-	
-	puts "There are still #{motorcycles} to repair."
 end
 
 
